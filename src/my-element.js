98493d5aa119df01697c4219d286065e420df2d6
gpaniati <gpaniati@gmail.com>
2024-11-17 19:05:11 -0300

    Primer commit

--- src/my-element.js
+++ src/my-element.js
@@ -2,24 +2,11 @@ import { LitElement, css, html } from 'lit'
 import litLogo from './assets/lit.svg'
 import viteLogo from '/vite.svg'
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 export class MyElement extends LitElement {
   static get properties() {
     return {
-      /**
-       * Copy for the read the docs hint.
-       */
-      docsHint: { type: String },
-
-      /**
-       * The number of times the button has been clicked.
-       */
-      count: { type: Number },
+        docsHint: { type: String },
+        count: { type: Number },
     }
   }
 
